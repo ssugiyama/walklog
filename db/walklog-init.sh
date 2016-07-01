e98b7bd8256b1769a9a3313b5556d7530d31d934
Shinichi Sugiyama <shin.sugi@gmail.com>
2016-07-02 02:10:01 +0900

    change default db name

--- db/walklog-init.sh
+++ db/walklog-init.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 cd /tmp
-psql --dbname=walklog -f schema.sql
+psql -f schema.sql
 ZIPFILE=$(ls *.zip | head -1)
 SHPFILE=${ZIPFILE%¥.zip}
 unzip $ZIPFILE
-shp2pgsql -s 4326 -g the_geom -I -W sjis $SHPFILE areas | psql --dbname=walklog
+shp2pgsql -s 4326 -g the_geom -I -W sjis $SHPFILE areas | psql 
