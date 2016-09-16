dad2f8f7a5df7e30ee4b6beba35f3d2a707f1e80
Shinichi Sugiyama <shin.sugi@gmail.com>
2016-09-17 00:58:05 +0900

    delete unused file and code

--- web/src/main.js
+++ web/src/main.js
@@ -7,8 +7,6 @@ import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 import { syncHistoryWithStore } from 'react-router-redux'
 import { store, routes } from './app';
 
-import BodyContainer from './body'
-
 const history = syncHistoryWithStore(browserHistory, store)
 
 ReactDOM.render(
