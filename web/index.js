3dea125a1d7f5e1c7784a1b86ebc93ae7f5bee78
Shinichi Sugiyama <shin.sugi@gmail.com>
2016-10-04 00:51:38 +0900

    add sitemap again

--- web/index.js
+++ web/index.js
@@ -11,7 +11,9 @@ var path       = require('path');
 var morgan     = require('morgan');
 var app        = express();
 var handleSSR  = require('./dist/ssr').default;
-
+var models     = require('./lib/models');
+var Walk       = models.sequelize.models.walks;
+var sitemap    = require('sitemap');
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
@@ -27,6 +29,27 @@ if ('development' == app.get('env')) {
 }
 
 app.use('/api', api);
+
+app.use('/sitemap.xml', function(req, res) {
+    var sm = sitemap.createSitemap({});
+    Walk.findAll({
+	attributes: ["id"],
+	where: {
+	    comment : {$ne: null}
+	}
+    }).then(function (results) {
+	results.forEach(function (row) {
+	    sm.add({ url: req.protocol + '://' + (req.get('X-Forwarded-Host') || req.get('Host')) + '/' + row.id });
+	});
+	sm.toXML( function (err, xml) {
+	    if (err) {
+		return res.status(500).end();
+	    }
+	    res.header('Content-Type', 'application/xml');
+	    res.send( xml );
+	});
+    });
+});
 
 app.use('/', handleSSR);
 
