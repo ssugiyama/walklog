202c5222c324b9e3a591f636408e827014a3d4bf
Shinichi Sugiyama <shin.sugi@gmail.com>
2018-04-18 02:40:40 +0900

    fix search api error

--- web/lib/api.js
+++ web/lib/api.js
@@ -13,141 +13,142 @@ const api = express.Router();
 module.exports = api;
 
 const searchFunc = params => {
-    const order_hash = {
-        'newest_first'       : ['date', 'desc'],
-        'oldest_first'       : 'date',
-        'longest_first'      : ['length', 'desc'],
-        'shortest_first'     : 'length',
-        'easternmost_first'  : [sequelize.fn('st_xmax', sequelize.col('path')),  'desc'],
-        'westernmost_first'  : sequelize.fn('st_xmin', sequelize.col('path')),
-        'southernmost_first' : sequelize.fn('st_ymin', sequelize.col('path')),
-        'northernmost_first' : [sequelize.fn('st_ymax', sequelize.col('path')), 'desc'],
-        'nearest_first'      : sequelize.literal('distance'),
-    };
-    const where = [];
-    const order = order_hash[params.order || 'newest_first'];
-    const attributes = ['id', 'date', 'title', 'comment', 'path', 'length', 'user_id'];
-    if (params.id) {
-        where.push({id: params.id});
-    }
-    else if (params.date) {
-        where.push({date: params.date});
-    }
-    else {
-        if (params.user) {
-            where.push({user_id: parseInt(params.user)});
-        }
-        if (params.year) {
-            where.push(sequelize.where(sequelize.fn('date_part', 'year', sequelize.col('date')), parseInt(params.year)));
-        }
-        if (params.month) {
-            where.push(sequelize.where(sequelize.fn('date_part', 'month', sequelize.col('date')), parseInt(params.month)));
-        }
-
-        if (params.filter == 'neighborhood') {
-            const latitude  = parseFloat(params.latitude);
-            const longitude = parseFloat(params.longitude);
-            const radius    = parseFloat(params.radius);
-            const dlat      = radius*180/Math.PI/models.EARTH_RADIUS;
-            const mlat      = latitude > 0 ? latitude + dlat : latitude - dlat;
-            const dlon      = dlat/Math.cos(mlat/180*Math.PI);
-            const center    = Walk.getPoint(longitude, latitude);
-            const lb        = Walk.getPoint(longitude-dlon, latitude-dlat);
-            const rt        = Walk.getPoint(longitude+dlon, latitude+dlat);
-            where.push(sequelize.where(sequelize.fn('st_makebox2d', lb, rt), {
-                [Op.overlap]: sequelize.col('path')
-            }));
-            where.push(sequelize.where(sequelize.fn('st_distance', sequelize.col('path'), center, true), {
-                [Op.lte]: radius
-            }));
-        }
-        else if (params.filter == 'cities') {
-            if (!params.cities) {
-                res.json({
-                    count: 0,
-                    rows: []
-                });
-                return;
-            }
-            const cities = params.cities.split(/,/).map(function (elm) { return `'${elm}'`; }).join(',');
-            where.push(sequelize.literal(`EXISTS (SELECT * FROM areas WHERE jcode IN (${cities}) AND path && the_geom AND ST_Intersects(path, the_geom))`));
-        }
-        else if (params.filter == 'crossing') {
-            if (!params.searchPath) {
-                res.json({
-                    count: 0,
-                    rows: []
-                });
-                return;
-            }
-            const linestring = Walk.decodePath(params.searchPath);
-            where.push({ 
-                path: {
-                    [Op.overlap]: linestring
-                }
-            });
-            where.push(sequelize.fn('ST_Intersects', sequelize.col('path'), linestring));
-        }
-        else if (params.filter == 'hausdorff') {
-            if (!params.searchPath) {
-                res.json({
-                    count: 0,
-                    rows: []
-                });
-                return;
-            }
-            const max_distance = params.max_distance || 4000;
-            const linestring = Walk.decodePath(params.searchPath);
-            const extent     = Walk.getPathExtent(params.searchPath);
-            const dlat       = max_distance*180/Math.PI/models.EARTH_RADIUS;
-            const mlat       = Math.max(Math.abs(extent.ymax + dlat), Math.abs(extent.ymin-dlat));
-            const dlon       = dlat/Math.cos(mlat/180*Math.PI);
-            const lb         = Walk.getPoint(extent.xmin-dlon, extent.ymin-dlat);
-            const rt         = Walk.getPoint(extent.xmax+dlon, extent.ymax+dlat);
-
-            attributes.push([`ST_HausdorffDistance(ST_Transform(path, ${models.SRID_FOR_SIMILAR_SEARCH}), ST_Transform('${linestring}'::Geometry, ${models.SRID_FOR_SIMILAR_SEARCH}))/1000`, 'distance']);
-            where.push(sequelize.fn('ST_Within', sequelize.col('path'), sequelize.fn('ST_SetSRID', sequelize.fn('ST_MakeBox2d', lb, rt), models.SRID)));
-            where.push(sequelize.where(
-                        sequelize.fn('ST_HausdorffDistance', 
-                            sequelize.fn('ST_Transform', sequelize.col('path'), models.SRID_FOR_SIMILAR_SEARCH), 
-                            sequelize.fn('ST_Transform', sequelize.fn('st_geomfromtext', linestring), models.SRID_FOR_SIMILAR_SEARCH)), {
-                                [Op.lt]: max_distance
-                            }));
-        }
-        else if (params.filter == 'frechet') {
-            if (!params.searchPath) {
-                res.json({
-                    count: 0,
-                    rows: []
-                });
-                return;
-            }
-            const max_distance = params.max_distance || 4000;
-            const linestring = Walk.decodePath(params.searchPath);
-            const sp         = Walk.getStartPoint(params.searchPath);
-            const ep         = Walk.getEndPoint(params.searchPath);
-            const dlat       = max_distance*180/Math.PI/models.EARTH_RADIUS;
-            const mlat       = Math.max(Math.abs(sp[1] + dlat), Math.abs(sp[1] - dlat), Math.abs(ep[1] + dlat), Math.abs(ep[1] - dlat));
-            const dlon       = dlat/Math.cos(mlat/180*Math.PI);
-            const slb         = Walk.getPoint(sp[0]-dlon, sp[1]-dlat);
-            const srt         = Walk.getPoint(sp[0]+dlon, sp[1]+dlat);
-            const elb         = Walk.getPoint(ep[0]-dlon, ep[1]-dlat);
-            const ert         = Walk.getPoint(ep[0]+dlon, ep[1]+dlat);
-
-            attributes.push([`ST_FrechetDistance(ST_Transform(path, ${models.SRID_FOR_SIMILAR_SEARCH}), ST_Transform('${linestring}'::Geometry, ${models.SRID_FOR_SIMILAR_SEARCH}))/1000`, 'distance']);
-            where.push(sequelize.fn('ST_Within', sequelize.fn('ST_StartPoint', sequelize.col('path')), sequelize.fn('ST_SetSRID', sequelize.fn('ST_MakeBox2d', slb, srt), models.SRID)));
-            where.push(sequelize.fn('ST_Within', sequelize.fn('ST_EndPoint', sequelize.col('path')), sequelize.fn('ST_SetSRID', sequelize.fn('ST_MakeBox2d', elb, ert), models.SRID)));
-            where.push(sequelize.where(
-                        sequelize.fn('ST_FrechetDistance',
-                            sequelize.fn('ST_Transform', sequelize.col('path'), models.SRID_FOR_SIMILAR_SEARCH),
-                            sequelize.fn('ST_Transform', sequelize.fn('st_geomfromtext', linestring), models.SRID_FOR_SIMILAR_SEARCH)), {
-                                [Op.lt]: max_distance
-                            }));
-        }
-    }
-    const limit  = parseInt(params.limit) || 20;
-    const offset = parseInt(params.offset) || 0;
     return new Promise((resolve, reject) => {
+        const order_hash = {
+            'newest_first'       : ['date', 'desc'],
+            'oldest_first'       : 'date',
+            'longest_first'      : ['length', 'desc'],
+            'shortest_first'     : 'length',
+            'easternmost_first'  : [sequelize.fn('st_xmax', sequelize.col('path')),  'desc'],
+            'westernmost_first'  : sequelize.fn('st_xmin', sequelize.col('path')),
+            'southernmost_first' : sequelize.fn('st_ymin', sequelize.col('path')),
+            'northernmost_first' : [sequelize.fn('st_ymax', sequelize.col('path')), 'desc'],
+            'nearest_first'      : sequelize.literal('distance'),
+        };
+        const where = [];
+        const order = order_hash[params.order || 'newest_first'];
+        const attributes = ['id', 'date', 'title', 'comment', 'path', 'length', 'user_id'];
+        if (params.id) {
+            where.push({id: params.id});
+        }
+        else if (params.date) {
+            where.push({date: params.date});
+        }
+        else {
+            if (params.user) {
+                where.push({user_id: parseInt(params.user)});
+            }
+            if (params.year) {
+                where.push(sequelize.where(sequelize.fn('date_part', 'year', sequelize.col('date')), parseInt(params.year)));
+            }
+            if (params.month) {
+                where.push(sequelize.where(sequelize.fn('date_part', 'month', sequelize.col('date')), parseInt(params.month)));
+            }
+
+            if (params.filter == 'neighborhood') {
+                const latitude  = parseFloat(params.latitude);
+                const longitude = parseFloat(params.longitude);
+                const radius    = parseFloat(params.radius);
+                const dlat      = radius*180/Math.PI/models.EARTH_RADIUS;
+                const mlat      = latitude > 0 ? latitude + dlat : latitude - dlat;
+                const dlon      = dlat/Math.cos(mlat/180*Math.PI);
+                const center    = Walk.getPoint(longitude, latitude);
+                const lb        = Walk.getPoint(longitude-dlon, latitude-dlat);
+                const rt        = Walk.getPoint(longitude+dlon, latitude+dlat);
+                where.push(sequelize.where(sequelize.fn('st_makebox2d', lb, rt), {
+                    [Op.overlap]: sequelize.col('path')
+                }));
+                where.push(sequelize.where(sequelize.fn('st_distance', sequelize.col('path'), center, true), {
+                    [Op.lte]: radius
+                }));
+            }
+            else if (params.filter == 'cities') {
+                if (!params.cities) {
+                    resolve({
+                        count: 0,
+                        rows: []
+                    });
+                    return;
+                }
+                const cities = params.cities.split(/,/).map(function (elm) { return `'${elm}'`; }).join(',');
+                where.push(sequelize.literal(`EXISTS (SELECT * FROM areas WHERE jcode IN (${cities}) AND path && the_geom AND ST_Intersects(path, the_geom))`));
+            }
+            else if (params.filter == 'crossing') {
+                if (!params.searchPath) {
+                    resolve({
+                        count: 0,
+                        rows: []
+                    });
+                    return;
+                }
+                const linestring = Walk.decodePath(params.searchPath);
+                where.push({ 
+                    path: {
+                        [Op.overlap]: linestring
+                    }
+                });
+                where.push(sequelize.fn('ST_Intersects', sequelize.col('path'), linestring));
+            }
+            else if (params.filter == 'hausdorff') {
+                if (!params.searchPath) {
+                    resolve({
+                        count: 0,
+                        rows: []
+                    });
+                    return;
+                }
+                const max_distance = params.max_distance || 4000;
+                const linestring = Walk.decodePath(params.searchPath);
+                const extent     = Walk.getPathExtent(params.searchPath);
+                const dlat       = max_distance*180/Math.PI/models.EARTH_RADIUS;
+                const mlat       = Math.max(Math.abs(extent.ymax + dlat), Math.abs(extent.ymin-dlat));
+                const dlon       = dlat/Math.cos(mlat/180*Math.PI);
+                const lb         = Walk.getPoint(extent.xmin-dlon, extent.ymin-dlat);
+                const rt         = Walk.getPoint(extent.xmax+dlon, extent.ymax+dlat);
+
+                attributes.push([`ST_HausdorffDistance(ST_Transform(path, ${models.SRID_FOR_SIMILAR_SEARCH}), ST_Transform('${linestring}'::Geometry, ${models.SRID_FOR_SIMILAR_SEARCH}))/1000`, 'distance']);
+                where.push(sequelize.fn('ST_Within', sequelize.col('path'), sequelize.fn('ST_SetSRID', sequelize.fn('ST_MakeBox2d', lb, rt), models.SRID)));
+                where.push(sequelize.where(
+                            sequelize.fn('ST_HausdorffDistance', 
+                                sequelize.fn('ST_Transform', sequelize.col('path'), models.SRID_FOR_SIMILAR_SEARCH), 
+                                sequelize.fn('ST_Transform', sequelize.fn('st_geomfromtext', linestring), models.SRID_FOR_SIMILAR_SEARCH)), {
+                                    [Op.lt]: max_distance
+                                }));
+            }
+            else if (params.filter == 'frechet') {
+                if (!params.searchPath) {
+                    resolve({
+                        count: 0,
+                        rows: []
+                    });
+                    return;
+                }
+                const max_distance = params.max_distance || 4000;
+                const linestring = Walk.decodePath(params.searchPath);
+                const sp         = Walk.getStartPoint(params.searchPath);
+                const ep         = Walk.getEndPoint(params.searchPath);
+                const dlat       = max_distance*180/Math.PI/models.EARTH_RADIUS;
+                const mlat       = Math.max(Math.abs(sp[1] + dlat), Math.abs(sp[1] - dlat), Math.abs(ep[1] + dlat), Math.abs(ep[1] - dlat));
+                const dlon       = dlat/Math.cos(mlat/180*Math.PI);
+                const slb         = Walk.getPoint(sp[0]-dlon, sp[1]-dlat);
+                const srt         = Walk.getPoint(sp[0]+dlon, sp[1]+dlat);
+                const elb         = Walk.getPoint(ep[0]-dlon, ep[1]-dlat);
+                const ert         = Walk.getPoint(ep[0]+dlon, ep[1]+dlat);
+
+                attributes.push([`ST_FrechetDistance(ST_Transform(path, ${models.SRID_FOR_SIMILAR_SEARCH}), ST_Transform('${linestring}'::Geometry, ${models.SRID_FOR_SIMILAR_SEARCH}))/1000`, 'distance']);
+                where.push(sequelize.fn('ST_Within', sequelize.fn('ST_StartPoint', sequelize.col('path')), sequelize.fn('ST_SetSRID', sequelize.fn('ST_MakeBox2d', slb, srt), models.SRID)));
+                where.push(sequelize.fn('ST_Within', sequelize.fn('ST_EndPoint', sequelize.col('path')), sequelize.fn('ST_SetSRID', sequelize.fn('ST_MakeBox2d', elb, ert), models.SRID)));
+                where.push(sequelize.where(
+                            sequelize.fn('ST_FrechetDistance',
+                                sequelize.fn('ST_Transform', sequelize.col('path'), models.SRID_FOR_SIMILAR_SEARCH),
+                                sequelize.fn('ST_Transform', sequelize.fn('st_geomfromtext', linestring), models.SRID_FOR_SIMILAR_SEARCH)), {
+                                    [Op.lt]: max_distance
+                                }));
+            }
+        }
+        const limit  = parseInt(params.limit) || 20;
+        const offset = parseInt(params.offset) || 0;
+    
         Walk.findAndCountAll({
             attributes : attributes,
             order  : [order],
