47d65f7385451520d4233b0a99bc7c27ad8ce702
杉山真一 <sugi@chubabook3.local>
2019-05-18 13:08:16 +0900

    delete docker-db-push

--- docker-db-push.sh
+++
@@ -1,9 +0,0 @@
-
-
-#! /bin/sh
-
-# walklog_web is pushed by circleci
-
-docker login -u $DOCKER_USER -p $DOCKER_PASS
-docker build -t $DOCKER_USER/walklog_db db
-docker push $DOCKER_USER/walklog_db:latest
