663f0ff309d7b330cd861c7dab8f7a80b5942419
Shinichi SUGIYAMA <shin.sugi@gmail.com>
2022-08-30 00:13:35 +0900

    fix ssr template

--- web/src/components/ssr.js
+++ web/src/components/ssr.js
@@ -24,7 +24,7 @@ const definePreloadedStateAndConfig = state => raw(
 const Wrapper = props => (
     <html lang="en" style={{ height: '100%' }}>
         <head>
-            <meta charset="utf-8" />
+            <meta charSet="utf-8" />
             <meta name="description" content={props.description} />
             <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no, viewport-fit=cover" />
             <meta name="apple-mobile-web-app-capable" content="yes" />
