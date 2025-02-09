52a0e4b1846930b8d9ed162edcc4628f23c05e13
Shinichi SUGIYAMA <shin.sugi@gmail.com>
2025-02-10 02:52:01 +0900

    purify dom

--- web/shared/components/item-box.js
+++ web/shared/components/item-box.js
@@ -19,6 +19,7 @@ import Avatar from '@mui/material/Avatar';
 import NoSsr from '@mui/material/NoSsr';
 import SwipeableViews from 'react-swipeable-views';
 import { push } from '@lagunovsky/redux-react-router';
+import DOMPurify from 'dompurify';
 import fetchWithAuth from '../fetch-with-auth';
 import { idToUrl } from '../app';
 import PanoramaBox from './panorama-box';
@@ -64,7 +65,7 @@ const ItemBox = () => {
         image;
     if (data) {
         title = `${data.date} : ${data.title} (${data.length.toFixed(1)} km)`;
-        createMarkup = () => ({ __html: marked.parse(data.comment || '') });
+        createMarkup = () => ({ __html: DOMPurify.sanitize(marked.parse(data.comment || ''))});
         image = data.image;
         users.forEach((u) => {
             if (u.uid === data.uid) dataUser = u;
