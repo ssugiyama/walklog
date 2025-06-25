61b97b1cdc35875685a527cf4655454452cf424e
Shinichi SUGIYAMA <shin.sugi@gmail.com>
2025-06-26 00:36:34 +0900

    adopt react-markdown

---
+++ web/__mocks__/react-markdown.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+// react-markdownのモック
+const ReactMarkdown = ({ children }) => {
+  return React.createElement('div', { 'data-testid': 'react-markdown' }, children);
+};
+
+export default ReactMarkdown;
