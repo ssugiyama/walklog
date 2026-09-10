b7e2fa26ae1319a06f630f620f0dce85f0e753d3
sugiyama-shinichi <sugiyama-shinichi@kayac.com>
2026-09-10 14:27:26 +0900

    style: apply formatter to meta-utils.ts

--- web/lib/utils/meta-utils.ts
+++ web/lib/utils/meta-utils.ts
@@ -1,8 +1,4 @@
-export const idToShowUrl = (
-  id: string | number,
-  params?: URLSearchParams,
-) => `/show/${id}${params ? `?${params.toString()}` : ''}`
-export const idToEditUrl = (
-  id: string | number,
-  params?: URLSearchParams,
-) => `/edit/${id}${params ? `?${params.toString()}` : ''}`
+export const idToShowUrl = (id: string | number, params?: URLSearchParams) =>
+  `/show/${id}${params ? `?${params.toString()}` : ''}`
+export const idToEditUrl = (id: string | number, params?: URLSearchParams) =>
+  `/edit/${id}${params ? `?${params.toString()}` : ''}`
